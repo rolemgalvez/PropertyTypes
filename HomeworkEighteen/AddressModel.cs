a319c145958b71e44b184883d2c19c5490a5d28d
Rolem Galvez <rolemgalvez@gmail.com>
2023-04-29 17:41:08 +0800

    Add project files.

---
+++ HomeworkEighteen/AddressModel.cs
@@ -0,0 +1,28 @@
+﻿namespace HomeworkEighteen
+{
+    public class AddressModel
+    {
+        public string Street { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string FullAddress
+        {
+            get
+            {
+                return $"{Street}, {Address}, {City}, {State}, {ZipCode}";
+            }
+        }
+
+        public AddressModel()
+        {
+
+        }
+
+        public AddressModel(string zipCode)
+        {
+            ZipCode = zipCode;
+        }
+    }
+}
---
+++ HomeworkEighteen/Program.cs
@@ -0,0 +1,23 @@
+﻿using HomeworkEighteen;
+
+Console.WriteLine("Homework Eighteen");
+Console.WriteLine("-----------------");
+Console.WriteLine();
+
+AddressModel address1 = new AddressModel();
+address1.Street = "Kahitsan";
+address1.Address = "Ayayay";
+address1.City = "Pasig";
+address1.State = "Dono";
+address1.ZipCode = "123";
+
+AddressModel address2 = new AddressModel("456");
+address2.Street = "Ayono";
+address2.Address = "Tabi";
+address2.City = "Marikina";
+address2.State = "Dale";
+
+Console.WriteLine(address1.FullAddress);
+Console.WriteLine(address2.FullAddress);
+
+Console.ReadLine();
